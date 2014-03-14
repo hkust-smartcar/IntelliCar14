cfd673a0f17e1c8d9f1a7e043dca95bd2866ec35
Ming Ming <mint2648@gmail.com>
2014-03-15 02:01:55 +0800

    Add Car for Linear CCD group

---
+++ src/linear_ccd/car.cpp
@@ -0,0 +1,95 @@
+/*
+ * car.cpp
+ * Linear CCD car
+ *
+ * Author: Ming Tsang
+ * Copyright (c) 2014 HKUST SmartCar Team
+ */
+
+#include <cstdint>
+
+#include <libsc/com/bluetooth.h>
+#include <libsc/com/encoder.h>
+#include <libsc/com/led.h>
+#include <libsc/com/linear_ccd.h>
+#include <libsc/com/motor.h>
+#include <libsc/com/servo.h>
+#include <libutil/misc.h>
+
+#include "linear_ccd/car.h"
+
+#define SERVO_MID_DEGREE 90
+#define SERVO_AMPLITUDE 40
+#define SERVO_MAX_DEGREE (SERVO_MID_DEGREE + SERVO_AMPLITUDE)
+#define SERVO_MIN_DEGREE (SERVO_MID_DEGREE - SERVO_AMPLITUDE)
+
+using namespace libsc;
+
+namespace linear_ccd
+{
+
+Car::Car()
+		: m_encoder(0), m_leds{Led(0), Led(1), Led(2), Led(3)}, m_motor(0),
+		  m_servo(0)
+{
+	m_motor.SetPower(0);
+	SetMotorDirection(true);
+	m_servo.SetDegree(SERVO_MID_DEGREE);
+	m_bt.StartReceive();
+}
+
+void Car::SetMotorDirection(const bool is_forward)
+{
+	m_motor.SetClockwise(!is_forward);
+}
+
+void Car::AddMotorPowerTil(const uint16_t factor, const uint16_t max)
+{
+	const uint16_t curr_power = GetMotorPower();
+	if (curr_power < max)
+	{
+		SetMotorPower(libutil::Clamp<uint16_t>(0, curr_power + factor, max));
+	}
+}
+
+void Car::DropMotorPower(const uint16_t factor)
+{
+	m_motor.AddPower(-factor);
+}
+
+void Car::DropMotorPowerTil(const uint16_t factor, const uint16_t min)
+{
+	const uint16_t curr_power = GetMotorPower();
+	if (curr_power > min)
+	{
+		SetMotorPower(libutil::Clamp<uint16_t>(min, curr_power - factor, 1000));
+	}
+}
+
+void Car::TurnLeft(const uint8_t percentage)
+{
+	m_servo.SetDegree(SERVO_MID_DEGREE - (percentage * SERVO_AMPLITUDE / 100));
+}
+
+void Car::TurnRight(const uint8_t percentage)
+{
+	m_servo.SetDegree((percentage * SERVO_AMPLITUDE / 100) + SERVO_MID_DEGREE);
+}
+
+void Car::SetRightPercentage(const uint8_t percentage)
+{
+	m_servo.SetDegree((percentage * (SERVO_AMPLITUDE * 2) / 100)
+			+ SERVO_MIN_DEGREE);
+}
+
+bool Car::IsMotorForward() const
+{
+	return !m_motor.IsClockwise();
+}
+
+uint8_t Car::GetRightPercentge() const
+{
+	return (m_servo.GetDegree() - SERVO_MIN_DEGREE) * 100 / (SERVO_AMPLITUDE * 2);
+}
+
+}
---
+++ src/linear_ccd/car.h
@@ -0,0 +1,142 @@
+/*
+ * car.h
+ * Linear CCD car
+ *
+ * Author: Ming Tsang
+ * Copyright (c) 2014 HKUST SmartCar Team
+ */
+
+#ifndef LINEAR_CCD_CAR_H_
+#define LINEAR_CCD_CAR_H_
+
+#include <libsc/com/bluetooth.h>
+#include <libsc/com/encoder.h>
+#include <libsc/com/led.h>
+#include <libsc/com/linear_ccd.h>
+#include <libsc/com/motor.h>
+#include <libsc/com/servo.h>
+
+namespace linear_ccd
+{
+
+class Car
+{
+public:
+	Car();
+
+
+	/**
+	 * Start the wheel motor
+	 *
+	 * @param is_forward Is going forward or not
+	 * @param power Power scale in [0, 1000]
+	 */
+	void StartMotor(const bool is_forward, const uint16_t power)
+	{
+		SetMotorDirection(is_forward);
+		SetMotorPower(power);
+	}
+
+	void StopMotor()
+	{
+		SetMotorPower(0);
+	}
+
+	void SetMotorDirection(const bool is_forward);
+	void SetMotorPower(const uint16_t power)
+	{
+		m_motor.SetPower(power);
+	}
+
+	void AddMotorPower(const uint16_t factor)
+	{
+		m_motor.AddPower(factor);
+	}
+
+	void AddMotorPowerTil(const uint16_t factor, const uint16_t max);
+	void DropMotorPower(const uint16_t factor);
+	void DropMotorPowerTil(const uint16_t factor, const uint16_t min);
+
+	/**
+	 * Turn the robot to the left
+	 *
+	 * @param percentage Specifying how aggressively should the car turn,
+	 * in [0, 100], where passing 0 basically means going straight
+	 */
+	void TurnLeft(const uint8_t percentage);
+	void TurnRight(const uint8_t percentage);
+	/**
+	 * Combining TurnLeft() and TurnRight() into one
+	 *
+	 * @param percentage 0 means turning left, and 70 means slightly turning
+	 * right
+	 * @return
+	 */
+	void SetRightPercentage(const uint8_t percentage);
+	void GoStraight()
+	{
+		TurnLeft(0);
+	}
+
+	/**
+	 * Switch on/off the LEDs
+	 *
+	 * @param obj
+	 * @param id The id of the LED, [0, 3]
+	 * @param flag
+	 */
+	void SwitchLed(const uint8_t id, const bool flag)
+	{
+		m_leds[id].SetEnable(flag);
+	}
+
+	const bool* SampleCcd()
+	{
+		return m_ccd.SampleData();
+	}
+
+	void UartSendStr(const char *str)
+	{
+		m_bt.SendStr(str);
+	}
+
+	void UartSendBuffer(const uint8_t *buf, const uint32_t len)
+	{
+		m_bt.SendBuffer(buf, len);
+	}
+
+	bool UartPeekChar(char *out_ch)
+	{
+		return m_bt.PeekChar(out_ch);
+	}
+
+	bool IsMotorForward() const;
+	bool IsMotorStop() const
+	{
+		return !m_motor.GetPower();
+	}
+
+	uint16_t GetMotorPower() const
+	{
+		return m_motor.GetPower();
+	}
+
+	uint32_t GetEncoderCount()
+	{
+		return m_encoder.GetCount();
+	}
+
+	uint8_t GetRightPercentge() const;
+
+private:
+	libsc::Bluetooth m_bt;
+	libsc::Encoder m_encoder;
+	libsc::Led m_leds[4];
+	libsc::LinearCcd m_ccd;
+	libsc::Motor m_motor;
+	libsc::Servo m_servo;
+};
+
+}
+
+#endif /* LINEAR_CCD_CAR_H_ */

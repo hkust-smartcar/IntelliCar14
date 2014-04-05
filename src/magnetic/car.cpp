7aafbfd5dc6cf6156ee94223f5ee96cabb95ec0e
Ming Ming <mint2648@gmail.com>
2014-04-05 16:05:53 +0800

    Add Motor and Servo support

--- src/magnetic/car.cpp
+++ src/magnetic/car.cpp
@@ -13,15 +13,70 @@
 
 #include "magnetic/car.h"
 
+#define SERVO_MID_DEGREE 90
+#define SERVO_AMPLITUDE 56
+#define SERVO_MAX_DEGREE (SERVO_MID_DEGREE + SERVO_AMPLITUDE)
+#define SERVO_MIN_DEGREE (SERVO_MID_DEGREE - SERVO_AMPLITUDE)
+
 using namespace libsc;
 
 namespace magnetic
 {
 
 Car::Car()
-		: m_leds{Led(0), Led(1), Led(2), Led(3)}, m_uart(3, 115200)
+		: m_leds{Led(0), Led(1), Led(2), Led(3)}, m_motor{Motor(0), Motor(1)},
+		  m_servo(0), m_uart(3, 115200)
 {
+	SetMotorPower(0);
+	SetMotorDirection(true);
+	m_servo.SetDegree(SERVO_MID_DEGREE);
 	m_uart.StartReceive();
+}
+
+void Car::SetMotorDirection(const bool is_forward)
+{
+	m_motor[0].SetClockwise(!is_forward);
+	m_motor[1].SetClockwise(!is_forward);
+}
+
+void Car::AddMotorPowerTil(const uint16_t factor, const uint16_t max)
+{
+	const uint16_t curr_power = GetMotorPower();
+	if (curr_power < max)
+	{
+		SetMotorPower(libutil::Clamp<uint16_t>(0, curr_power + factor, max));
+	}
+}
+
+void Car::DropMotorPower(const uint16_t factor)
+{
+	m_motor[0].AddPower(-factor);
+	m_motor[1].AddPower(-factor);
+}
+
+void Car::DropMotorPowerTil(const uint16_t factor, const uint16_t min)
+{
+	const uint16_t curr_power = GetMotorPower();
+	if (curr_power > min)
+	{
+		SetMotorPower(libutil::Clamp<uint16_t>(min, curr_power - factor, 1000));
+	}
+}
+
+void Car::SetTurning(const int16_t percentage)
+{
+	const int16_t _percentage = libutil::Clamp<int16_t>(-100, percentage, 100);
+	m_servo.SetDegree(SERVO_MID_DEGREE + (_percentage * SERVO_AMPLITUDE / 100));
+}
+
+bool Car::IsMotorForward() const
+{
+	return !m_motor[0].IsClockwise();
+}
+
+uint8_t Car::GetRightPercentge() const
+{
+	return (m_servo.GetDegree() - SERVO_MIN_DEGREE) * 100 / (SERVO_AMPLITUDE * 2);
 }
 
 }

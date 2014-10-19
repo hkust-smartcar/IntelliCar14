04aca63dff9f623c70784dbf1ffb8ccc22c5c6c6
Ming Ming <mint2648@gmail.com>
2014-10-19 18:08:16 +0800

    Remove camera and magnetic (they should live in their own repo/branch)

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,7 @@
 
 #include <libsc/lib_guard.h>
 
-#if defined(K60_2014_CCD) || defined(K60_2013_CCD)
-	#include "linear_ccd/linear_ccd_app.h"
-#elif defined(K60_2014_CAMERA)
-	#include "camera/camera_app.h"
-#elif defined(K60_2014_MAGNETIC)
-	#include "magnetic/magnetic_app.h"
-#endif
+#include "linear_ccd/linear_ccd_app.h"
 
 namespace libbase
 {
@@ -32,13 +26,7 @@ Mcg::Config Mcg::GetMcgConfig()
 int main()
 {
 	LIBSC_GUARD();
-#if defined(K60_2014_CCD)
 	linear_ccd::LinearCcdApp app;
-#elif defined(CAMERA)
-	camera::CameraApp app;
-#elif defined(MAGNETIC)
-	magnetic::MagneticApp app;
-#endif
 	app.Run();
 	return 0;
 }
